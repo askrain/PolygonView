fc043b83d52574064b8d14a586e646109e335d92
miushuki <729783606@qq.com>
2017-07-20 11:02:09 +0800

    A demo about drag border of the  Polygon and redraw it.

---
+++ app/src/main/java/com/jsptpd/polygonview/MainActivity.java
@@ -0,0 +1,13 @@
+package com.jsptpd.polygonview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new MyPolygonEvent(this));
+    }
+}
